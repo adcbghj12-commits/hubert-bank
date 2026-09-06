b846fd23efaa18bfdbc4a5538a7941412fa5f098
adcbghj12-commits <adcbghj12@gmail.com>
2026-09-06 17:30:41 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+import './styles.css';
 
+export const metadata = {
+  title: 'Hubert Bank',
+  description: 'Ví điểm ảo trên Telegram',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="vi">
+      <body>{children}</body>
+    </html>
+  );
+}
